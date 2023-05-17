ac2daa9629fa42f4418e3e0a7dac9918ae247a77
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-17 21:28:55 +0600

    navber added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Navber from '../pages/shard/Navber/Navber';
 
 const Main = () => {
     return (
         <div>
+            <Navber></Navber>
             <Outlet></Outlet>
         </div>
     );
