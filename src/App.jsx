3ccb9661ed6672ae5f45dac619ff45f031d26095
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-17 21:23:53 +0600

    action figure client initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import HEader from './components/Header/HEader'
-import { Outlet } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <HEader></HEader>
-      <Outlet></Outlet>
+      <h1>Header</h1>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import Home from './components/Home/Home'
-import About from './components/About/About'
+import { RouterProvider} from 'react-router-dom'
+import router from './Routers/Routers'
 
-const router = createBrowserRouter([
-  {
-    path:'/',
-    element:<App></App>,
-    children:[
-      {
-        path: '/',
-        element:<Home></Home>
-      },
-      {
-        path:'/about',
-        element:<About></About>
-      }
-    ]
-  }
-])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router}></RouterProvider>
