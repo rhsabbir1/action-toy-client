8c5e0cf43e607aeded11f09c6a910fe561480417
rhsabbir1 <rhsabbir090@gmai.com>
2023-05-19 21:52:26 +0600

    add new toy mongoDb

--- src/pages/Home/ToysPage/AddAToys.jsx
+++ src/pages/Home/ToysPage/AddAToys.jsx
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContex } from './../../../AuthProvider/AuthProvider';
 
 const AddAToys = () => {
+
+    const {user} = useContext(AuthContex)
+
+    const handleAddToy = event =>{
+        event.preventDefault()
+        const form = event.target;
+
+
+        const toyname = form.toyName.value;
+        const pictureURL = form.pictureURL.value;
+        const sub_category = form.sub_category.value;
+        const price = form.price.value;
+        const rating = form.rating.value;
+        const quantity = form.quantity.value;
+        const description = form.description.value;
+
+        const addtoy = {
+            toyname,
+            seller : user?.displayName,
+            pictureURL,
+            sellerEmail : user?.email,
+            sub_category,
+            price,
+            rating,
+            quantity,
+            description
+        }
+
+        fetch('http://localhost:5000/products',{
+            method:"POST",
+            headers:{
+                'content-type':'application/json'
+            },
+            body:JSON.stringify(addtoy)
+        })
+        .then(res =>res.json())
+        .then(data =>{
+            console.log(data)
+        })
+        
+    }
+
     return (
         <div className=' bg-slate-500 py-4'>
             <h1 className='text-5xl text-center font-bold text-white'> Add Toys</h1>
 
-            <form className='ml-40'>
+            <form onSubmit={handleAddToy} className='ml-40'>
             <div className="card-body w-10/12 mx-auto">
                     <div className='grid md:grid-cols-2 gap-4'>
 
@@ -15,11 +58,13 @@ const AddAToys = () => {
                             </label>
                             <input type="text" name='toyName' placeholder="Toy Name" className="input input-bordered" />
                         </div>
+
+
                         <div className="form-control">
                             <label className="label">
                                 <span className=" text-white font-semibold">Seller name</span>
                             </label>
-                            <input type="text" name='seller' placeholder="Seller name" className="input input-bordered" />
+                            <input type="text" name='seller' defaultValue={user.displayName} placeholder="Seller name" className="input input-bordered" />
 
                         </div>
 
@@ -34,14 +79,14 @@ const AddAToys = () => {
                             <label className="label">
                                 <span className=" text-white font-semibold">Seller email</span>
                             </label>
-                            <input type="text" name='email' placeholder='Seller Email' className="input input-bordered" />
+                            <input type="text" name='email'  defaultValue={user?.email} placeholder='Seller Email' className="input input-bordered" />
                         </div>
 
                         <div className="form-control">
                             <label className="label">
                                 <span className=" text-white font-semibold">Sub Category</span>
                             </label>
-                            <input type="text" name='sub-category'  className="input input-bordered" placeholder='Sub Category'/>
+                            <input type="text" name='sub_category'  className="input input-bordered" placeholder='Sub Category'/>
                         </div>
 
                         <div className="form-control">
